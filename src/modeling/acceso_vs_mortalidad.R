362054c6052c1c661481dae382ef3fed3302255c
HAVB <avazquez@gmail.com>
2020-03-03 13:24:08 -0300

    modelo de mortalidad vs acceso ahora usando promedio de mortalidad de cinco años más recientes

--- src/modeling/acceso_vs_mortalidad.R
+++ src/modeling/acceso_vs_mortalidad.R
@@ -2,10 +2,19 @@ library(tidyverse)
 
 accesibilidad <- read_csv("data/processed/metricas/accesibilidad_espacios_verdes_localidades.csv") 
 
-mortalidad_AMBA <- read_csv("data/processed/DEIS/mortalidad_AMBA_2018.csv") %>% 
+mortalidad_AMBA_2014_2018 <- read_csv("data/processed/DEIS/mortalidad_AMBA_2014_2018.csv") %>% 
     mutate(tasa_m_infantil = (`Menores de 1`/`Nacidos Vivos`) * 1000,
            # Esto esta mal, necesitariamos la cantidad de partos. Pero para explorar lo hacemos
            tasa_m_maternal = (`Muertes Maternas`/`Nacidos Vivos`) * 1000)
+
+# tomamos el promedio de los 5 años
+
+mortalidad_AMBA_promedio_2014_2018 <- mortalidad_AMBA_2014_2018 %>% 
+    group_by(eph_aglome, localidade) %>% 
+    summarise(tasa_m_infantil_promedio = mean(tasa_m_infantil),
+              tasa_m_maternal_promedio = mean(tasa_m_maternal))
+
+
 
 ####
 
@@ -15,39 +24,51 @@ get_effect_m_infantil <- function(dframe) {
     decil = last(dframe$decil_NSE)
     
     dframe %>% 
-        {lm(tasa_m_infantil ~ tasa_acceso + total_ha_accesibles + m2_accesibles_per_capita, data = .)} %>% 
+        {lm(tasa_m_infantil_promedio ~ tasa_acceso , data = .)} %>% 
         broom::tidy() %>% 
-        mutate(p.value = round(p.value, 2)) %>% 
+        mutate(p.value = round(p.value, 3)) %>% 
         {cbind(decil, .)} %>% 
         filter(term != "(Intercept)")
 }
+
 
 plot_model <- function(dframe) {
     
     decil = last(dframe$decil_NSE)
     
-    p <- ggplot(dframe, aes(x = tasa_acceso, y = tasa_m_infantil)) +
+    p <- ggplot(dframe, aes(x = tasa_acceso, y = tasa_m_infantil_promedio)) +
         geom_point() +
+        geom_text(aes(label = localidade)) +
         geom_smooth(method = "lm") +
         labs(title = paste(decil))
 }
 
 
 # Encontramos que una mayor tasa de acceso a esapacios verdes esta correlacionada con una menor
-# mortandad infantil... para los estratos más altos de la población! Deciles NSE 7, 8, 9, 10
+# mortandad infantil... para todos los estratos de la población, excepto el más bajo!
 
 accesibilidad %>% 
+    filter(eph_aglome %in% c("CABA", "Partidos del GBA")) %>% 
     filter(!is.na(decil_NSE)) %>% 
-    right_join(mortalidad_AMBA) %>% 
+    right_join(mortalidad_AMBA_promedio_2014_2018) %>% 
     group_by(decil_NSE) %>% 
     group_split() %>% 
     map_df(get_effect_m_infantil)
+
+
+# Comparado contra una regresión de todos los deciles juntos, y el decil como predictor
+accesibilidad %>% 
+    filter(eph_aglome %in% c("CABA", "Partidos del GBA")) %>% 
+    filter(!is.na(decil_NSE)) %>% 
+    right_join(mortalidad_AMBA_promedio_2014_2018) %>% 
+    {lm(data = ., tasa_m_infantil_promedio ~ tasa_acceso + total_ha_accesibles + m2_accesibles_per_capita + decil_NSE)} %>% 
+    summary
 
 # Scatterplots
 
 accesibilidad %>% 
     filter(!is.na(decil_NSE)) %>% 
-    right_join(mortalidad_AMBA) %>% 
+    right_join(mortalidad_AMBA_promedio_2014_2018) %>% 
     group_by(decil_NSE) %>% 
     group_split() %>% 
     map(plot_model)
@@ -55,8 +76,8 @@ accesibilidad %>%
 
 ### Mortalidad maternal
 # Tambien encontramos que una mayor tasa de acceso a esapacios verdes esta correlacionada con una menor
-# tasa de mortandad materna / nacimientos... para los estratos más altos de la población! 
-# Deciles NSE 6, 7, 8, 9, 10
+# tasa de (mortandad materna / nacimientos)... excepto para los estratos más bajos de la población! 
+# todos menos deciles NSE 1 a 3
 
 
 get_effect_m_maternal <- function(dframe) {
@@ -64,9 +85,9 @@ get_effect_m_maternal <- function(dframe) {
     decil = last(dframe$decil_NSE)
     
     dframe %>% 
-        {lm(tasa_m_maternal ~ tasa_acceso + total_ha_accesibles + m2_accesibles_per_capita, data = .)} %>% 
+        {lm(tasa_m_maternal_promedio ~ tasa_acceso , data = .)} %>% 
         broom::tidy() %>% 
-        mutate(p.value = round(p.value, 2)) %>% 
+        mutate(p.value = round(p.value, 3)) %>% 
         {cbind(decil, .)} %>% 
         filter(term != "(Intercept)")
 }
@@ -74,7 +95,7 @@ get_effect_m_maternal <- function(dframe) {
 
 accesibilidad %>% 
     filter(!is.na(decil_NSE)) %>% 
-    right_join(mortalidad_AMBA) %>% 
+    right_join(mortalidad_AMBA_promedio_2014_2018) %>% 
     group_by(decil_NSE) %>% 
     group_split() %>% 
     map_df(get_effect_m_maternal)
